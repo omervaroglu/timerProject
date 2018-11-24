2f45b5a7161a53910c4fa4d38bd1400271c6b4bd
Omer <omer@Omer-MacBook-Pro.local>
2018-11-24 14:48:03 +0300

    Initial Commit

---
+++ timer project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  timer project
+//
+//  Created by Omer  on 24.11.2018.
+//  Copyright © 2018 Omer Varoglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
