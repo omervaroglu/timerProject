8a2b3bed07cdc587069b4431a6992f4c6b5d06c1
Omer <omer@Omer-MacBook-Pro.local>
2018-11-28 19:09:53 +0300

    initial commit

--- timer project/ViewController.swift
+++ timer project/ViewController.swift
@@ -9,12 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textLabel1: UILabel!
+    @IBOutlet weak var inputLabel: UILabel!
+    
+    
+    var timer = Timer()
+    var counter = 0
+    var secondcounter = 0
+    
+    @IBAction func stepper(_ sender: UIStepper) {
+        counter = Int(sender.value)
+        secondcounter = Int(sender.value)
+        inputLabel.text = String(counter)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    
+    @IBAction func start(_ sender: UIButton) {
+        print(counter)
+        print(secondcounter)
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.timerfunc), userInfo: nil, repeats: true)
+        if counter == 0{
+            timer.invalidate() // use for finish.
+            textLabel1.text = "time is up!"
+        }
+        counter = secondcounter
+        
+    }
+    
+    @objc func timerfunc() {
+        print("func \(counter)")
+        counter = counter - 1 // because textlabel show yourself after one second. I couldn't solve the problem so i could this.
+        textLabel1.text = String(counter)
+        
+        if (counter == 0 ){
+            timer.invalidate()
+            textLabel1.text = "time is up!"
+        }
+    }
 }
-
